7c0fac32288d9bd21f03820cf1eed940b09b2f82
Chingiz Shigayev <shigayevphone@gmail.com>
2022-06-13 10:05:43 +0400

    ref: function object creator - class Album

--- album.mjs
+++ album.mjs
@@ -1,47 +1,47 @@
-function Album() {
-  this.album = [];
-}
+class Album {
+  album = [];
 
-Album.prototype.deleteSong = function (...idList) {
-  idList.forEach((id) => {
-    this.album = this.album.filter((item) => item.id !== id);
-  });
-};
-
-Album.prototype.sortSongs = function (direct = "asc") {
-  if (direct === "asc") {
-    this.album.sort((a, b) =>
-      a.title > b.title ? 1 : a.title < b.title ? -1 : 0
-    );
-  } else {
-    this.album.sort((a, b) =>
-      a.title < b.title ? 1 : a.title > b.title ? -1 : 0
-    );
-  }
-};
-
-Album.prototype.showPlaylist = function () {
-  console.log(this.album);
-};
-
-Album.prototype.showFavoriteSongs = function (favorite = false) {
-  favorite
-    ? console.log(this.album.filter((song) => song.favorite))
-    : console.log(this.album.filter((song) => !song.favorite));
-};
-
-Album.prototype.showOneForSinger = function () {
-  let alToShow = this.album.reduce((newAlbum, current) => {
-    if (!newAlbum.some((x) => x.singer === current.singer)) {
-      newAlbum.push(current);
+  deleteSong = function (...idList) {
+    idList.forEach((id) => {
+      this.album = this.album.filter((item) => item.id !== id);
+    });
+  };
+  
+  sortSongs = function (direct = "asc") {
+    if (direct === "asc") {
+      this.album.sort((a, b) =>
+        a.title > b.title ? 1 : a.title < b.title ? -1 : 0
+      );
+    } else {
+      this.album.sort((a, b) =>
+        a.title < b.title ? 1 : a.title > b.title ? -1 : 0
+      );
     }
-    return newAlbum;
-  }, []);
-  console.log(alToShow);
-};
-
-Album.prototype.addSongs = function (...songs) {
-  this.album.push(...songs);
-};
+  };
+  
+  showPlaylist = function () {
+    console.log(this.album);
+  };
+  
+  showFavoriteSongs = function (favorite = false) {
+    favorite
+      ? console.log(this.album.filter((song) => song.favorite))
+      : console.log(this.album.filter((song) => !song.favorite));
+  };
+  
+  showOneForSinger = function () {
+    let alToShow = this.album.reduce((newAlbum, current) => {
+      if (!newAlbum.some((x) => x.singer === current.singer)) {
+        newAlbum.push(current);
+      }
+      return newAlbum;
+    }, []);
+    console.log(alToShow);
+  };
+  
+  addSongs = function (...songs) {
+    this.album.push(...songs);
+  };
+}
 
 export default Album;
