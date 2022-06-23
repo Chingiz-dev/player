783647429fdd2be82d6c9f7af19ad13c3abed102
Chingiz Shigayev <shigayevphone@gmail.com>
2022-06-23 15:06:58 +0400

    fix: if !localStorage, download from db.json

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import PlayerComponent from "./modules/player/player.component.mjs";
 import Playlist from "./modules/player/models/playlist.mjs";
-// import songsList from "../../db.json" assert { type: "json" };
+import songsList from "../../db.json" assert { type: "json" };
 import DetailedSong from "./modules/player/models/detailedSong.mjs";
 import Song from "./modules/player/models/song.mjs";
 import Store from "./modules/Store.mjs";
@@ -8,9 +8,12 @@ import Store from "./modules/Store.mjs";
 const myStore = new Store('playlist');
 // myStore.putToStore(JSON.stringify(songsList));
 // myStore.clearStore();
-const songsList1 = JSON.parse(myStore.getFromStore());
+let songsList1 = JSON.parse(myStore.getFromStore());
 // console.log(songsList1);
 // console.log(songsList);
+if (!songsList1){
+  songsList1 = songsList;
+}
 
 const entryPoint = document.querySelector(".root");
 
